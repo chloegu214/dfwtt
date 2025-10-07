67d13ab2cbe3a4e1095c5f537d7d5b08e7914dc3
larsang7x <lsangren@gmail.com>
2025-10-06 22:46:39 -0500

    Update: Closings section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -86,7 +86,7 @@ export default function AboutPage() {
       <section className="py-12 bg-yellow-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
-      {/* Location Closings Section */ }        
+      {/* Location Closings Section         
           <Alert className="border-black-200 bg-red-50">
              <AlertDescription className="text-red-800">
               <div className="space-y-2">
@@ -101,7 +101,7 @@ export default function AboutPage() {
           </Alert>
 
           <p>&nbsp;</p>
-       
+      */ }       
      
       {/* Upcoming Tournaments Section */}
           <Alert className="border-black-200 bg-yellow-50">
