6943f0f804e70b73abede21b733117c84148e9f6
larsang7x <lsangren@gmail.com>
2026-01-27 05:50:10 -0600

    Update: Closing section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -86,7 +86,7 @@ export default function AboutPage() {
       <section className="py-12 bg-yellow-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
-      { /*Location Closings Section */} 
+      { /*Location Closings Section  
            
           <Alert className="border-black-200 bg-red-50">
              <AlertDescription className="text-red-800">
@@ -95,13 +95,14 @@ export default function AboutPage() {
                   Location Closings:
                 </p>
                 <p>
-                  Both Cimarron and Timberglen TT clubs will be closed Monday (January 26)
+                  None
                 </p>
                </div>
             </AlertDescription>
           </Alert>
 
           <p>&nbsp;</p>
+      */}
            
       {/* Upcoming Tournaments Section */ }
  
