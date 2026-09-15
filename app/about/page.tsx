c1b7ade8a725712043acb9ffca0792f3e1ed9b91
larsang7x <lsangren@gmail.com>
2026-09-15 06:41:54 -0500

    Update: Tournaments section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -113,12 +113,19 @@ export default function AboutPage() {
                   Upcoming Tournaments:
                   </p>
                   <p>
+                   2026 DFWTT Fall Open on November 7 (Saturday) at the Cimarron Recreation Center in Irving.&nbsp;
+                   <a href= "/documents/DFWTT26FallOpnR3.pdf"
+                      className="font-medium text-blue-600 hover:text-blue-800 underline"                  
+                      target="_blank">Entry Form
+                   </a>
+                 </p> 
+                 <p>
                    2026 Chinese Double Ten Celebration tournament on September 19 (Saturday) at the Cimarron Recreation Center in Irving.&nbsp;
                    <a href= "/documents/2026DblTen.pdf"
                       className="font-medium text-blue-600 hover:text-blue-800 underline"                  
                       target="_blank">Entry Form
                    </a>
-                 </p>                                        
+                 </p>                                                                     
               </div>
              </AlertDescription>  
           </Alert>  
